6eaf9a4c70d15ab75cbe2a57b58ec85359857c64
Tjardo <dev@tjardo.com>
2022-08-03 14:45:43 +0200

    Database transaction example

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,16 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\App;
 use App\Models\User;
 use App\View;
+use Throwable;
 
 class HomeController
 {
     public function index(): View
     {
+        $db = App::db();
+
         $user = new User();
 
-        $lastInsertedUserId = $user->create('demo7@example.org', 'Demo', 'Seven', false);
+        try {
+            $db->beginTransaction();
+
+            $lastInsertedUserId = $user->create('demo7@example.org', 'Demo', 'Seven', false);
+
+            $db->commit();
+        } catch (Throwable $exception) {
+            if ($db->inTransaction) {
+                $db->rollback();
+            }
+
+            throw $exception;
+        }
+
         $user = $user->find($lastInsertedUserId);
 
         return View::make('welcome', [
