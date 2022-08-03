aa963eebcf1d6ee179ec55d5273715e83df1d8cb
Tjardo <dev@tjardo.com>
2022-08-03 11:49:50 +0200

    Add GET POST support to router

---
+++ app/Controllers/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class TaskController
+{
+    public function index(): string
+    {
+        return 'Task';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\TaskController;
 use App\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $router = new Router();
 
-$router->register('/', [HomeController::class, 'index']);
+$router
+    ->get('/', [HomeController::class, 'index'])
+    ->get('/task', [TaskController::class, 'index']);
 
-$router->register('/test', function () {
+$router->register('get', '/test', function () {
     echo 'Test';
 });
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
