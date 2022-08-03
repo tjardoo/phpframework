18cf9fe93c930e753ddd0623b3da54e8873d9993
Tjardo <dev@tjardo.com>
2022-08-03 11:54:34 +0200

    Add 404 response code

--- public/index.php
+++ public/index.php
@@ -2,18 +2,28 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\TaskController;
+use App\Exceptions\RouteNotFoundException;
 use App\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$router = new Router();
+try {
+    $router = new Router();
 
-$router
-    ->get('/', [HomeController::class, 'index'])
-    ->get('/task', [TaskController::class, 'index']);
+    $router
+        ->get('/', [HomeController::class, 'index'])
+        ->get('/task', [TaskController::class, 'index']);
 
-$router->register('get', '/test', function () {
-    echo 'Test';
-});
+    $router->register('get', '/test', function () {
+        echo 'Test';
+    });
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+    echo $router->resolve(
+        $_SERVER['REQUEST_URI'],
+        strtolower($_SERVER['REQUEST_METHOD']),
+    );
+} catch (RouteNotFoundException) {
+    http_response_code(404);
+
+    echo '404';
+}
