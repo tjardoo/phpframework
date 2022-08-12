58072547808f945c42c4718b6e2b894cab81011b
Tjardo <dev@tjardo.com>
2022-08-12 17:52:44 +0200

    Add Eloquent event example

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,21 +6,20 @@ namespace App\Controllers;
 
 use App\Models\User;
 use App\View;
+use Carbon\Carbon;
 
 class HomeController
 {
     public function index(): View
     {
-        User::query()
-            ->where('id', 1)
-            ->update([
-                'email' => 'demo30@example.org',
-                'last_name' => 'Thirty',
+        $user = User::query()
+            ->create([
+                'email' => 'demo40@example.org',
+                'first_name' => 'Demo',
+                'last_name' => 'Fourty',
+                'is_active' => false,
+                // 'created_at' => Carbon::today()->startOfYear(),
             ]);
-
-        $user = User::query()->findOrFail(1);
-
-        dd($user);
 
         return View::make('welcome', [
             'foo' => 'bar',
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
+    public static function booted()
+    {
+        static::creating(function (User $user) {
+            if ($user->isClean('created_at')) {
+                $user->created_at = Carbon::now()->addDay(1);
+            }
+        });
+    }
+
     public $timestamps = false;
+
+    protected $guarded = [];
 
     protected $casts = [
         'is_active' => 'boolean',
